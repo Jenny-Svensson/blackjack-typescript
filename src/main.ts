7f96141344291c0f1ccfc22b23041383b13d0219
Jenny Svensson <jenny.svensson@medieinstitutet.se>
2023-05-06 12:31:46 +0200

    pseudo code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,42 @@
+/** pseudo code 
+ * Player should get 2 cards at the beginning
+ * Player can either hit more cards
+ * Player can stand
+ * Player can double up (if the total sum of player[] is between 9-11)
+ * Player can split (if the 2 first cards are the same value)
+ * 
+ * if player gets more than 21, player lose and PlayAgain-button shows. 
+ * PlayAgain-button should start the game over again
+ * 
+ * if the first two cards are either an Ace OR 10, J, Q, K you should get BlackJack
+ * it doesnt matter if the first card are an Ace or 10, J, Q, K &&& if the second card is an Ace or 10, J, Q, K
+ * If player gets BlackJack, all the button = disable and Dealer takes a card.
+ * 
+ * Dealer gets 1 card at the beginning
+ * When player hits stand, the dealer should take another card
+ * The dealer takes card until 17 - 21. 
+ * If 17-21, dealer stand.
+ * If the total sum of dealer[] is bigger than 21, Dealer lose and Player wins.
+ * 
+ * If total sum of player[] is > than dealer[] AND < 21, player wins.
+ * If total sum of dealer[] is > than player [] AND < 21, dealer wins.
+ * 
+ * if player gets blackjack, and dealer has 2-9, player wins.
+ * if player gets blackjack, and dealer has 10, J, Q, K, dealer takes one more card.
+ * If dealer gets an Ace on the second card, it is push.
+ * Otherwise, player wins (if the card is not an Ace).
+ * 
+ * 
+*/
+
+
 const app = document.getElementById('app');
 
 type cardValue = {value: string, altValue: number};
 
 const symbols: string[] = ["♥", "♦", "♣", "♠"];
 const valueCards: cardValue[] = [
-  {value: "A", altValue: 11},
+  {value: "A", altValue: 1 | 11},
   {value: "J", altValue: 10},
   {value: "Q", altValue: 10},
   {value: "K", altValue: 10},
@@ -54,5 +86,33 @@ function getCardForPlayer() {
   card.innerHTML = "Player: " + cardForPlayer.value + " " + symbol;
   app?.appendChild(card);
 
-  console.log(player)
+  sumUpTotalPlayer();
+
+}
+
+function sumUpTotalPlayer() {
+  let sum =  player.reduce((total, value) => total + value, 0);
+  console.log( "Player total: " + sum); // sum up the total in player array
+
+  // if sum is BIGGER than 21 = too much + disable hitbtn
+  if (sum > 21) {
+    console.log('too much')
+    // stop the game, disable all buttons and show play again button
+    let playAgainText = document.createElement('p');
+    playAgainText.innerText = "Too much! Play again?"
+
+    let resetBtn = document.createElement('button');
+    resetBtn.innerHTML = "Play again";
+
+    hitBtn.disabled = true;
+
+    app?.appendChild(playAgainText);
+    app?.appendChild(resetBtn);
+
+  // if sum is 21, disable the hit card btn and stand
+  } if (sum === 21) { 
+    console.log('21!!!');
+
+  // if the 2 first cards is Ace AND 10 | J(11) | Q(12) | K(13), you get blackjack! 
+  } 
 }
